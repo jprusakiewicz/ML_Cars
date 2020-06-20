f52503cd4b9f3993fa9ae331b25df8cbc8407ee5
Kuba PrusakiewiczPC <jakubprusakiewicz@gmail.com>
2020-06-20 16:04:58 +0200

    feat countdown, but without display

--- ML_Cars/Assets/RaceController.cs
+++ ML_Cars/Assets/RaceController.cs
@@ -9,12 +9,54 @@ public class RaceController : MonoBehaviour
     public Text laps;
     private int lapCount = 1;
     private int currentLap = 0;
-    private bool finnished = false; 
+    private bool finnished = false;
+    private int integer;
+    private bool canDrive = false;
+    private string minutes = "0";
+    private string _seconds = "0";
 
     private float startTime;
     // Start is called before the first frame update
     void Start()
     {
+        StartCoroutine("Countdown");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canDrive)
+        {
+            if (finnished)
+                return;
+            float t = Time.time - startTime;
+            minutes = ((int) t / 60).ToString();
+            _seconds = (t % 60).ToString("f2");
+            
+        }
+        timer.text = minutes + ":" + _seconds;
+        laps.text = "lap: " + currentLap.ToString() + "/" + lapCount.ToString();
+    }
+
+    // private void CountDown()
+    // {
+    //     
+    // }
+
+    IEnumerator Countdown()
+    {
+        float duration = 3f; // 3 seconds you can change this to
+        //to whatever you want
+        float totalTime = 0;
+        while(totalTime <= duration)
+        {
+            //countdownImage.fillAmount = totalTime / duration;
+            totalTime += Time.deltaTime;
+            integer = (int)totalTime; /* choose how to quantize this */
+            /* convert integer to string and assign to text */
+            yield return null;
+        }
         GameObject.Find("SportsCar_1").SendMessage("EnableControl");
         GameObject.Find("SportsCar_2").SendMessage("EnableControl");
         GameObject.Find("SportsCar_3").SendMessage("EnableControl");
@@ -22,21 +64,8 @@ public class RaceController : MonoBehaviour
         GameObject.Find("SportsCar_5").SendMessage("EnableControl");
         GameObject.Find("SportsCar_player").SendMessage("EnableControl");
         startTime = Time.time;
+        canDrive = true;
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if(finnished)
-            return;
-        float t = Time.time - startTime;
-        string minutes = ((int) t / 60).ToString();
-        string seconds = (t % 60).ToString("f2");
-        timer.text = minutes + ":" + seconds;
-        laps.text = "lap: "+currentLap.ToString()+"/"+ lapCount.ToString();
-        
-    }
-
     public int GetNumberOfLaps()
     {
         return lapCount;
