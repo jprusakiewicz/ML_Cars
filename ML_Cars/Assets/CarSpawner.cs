4210e8fa30cc65ff5beaeb18a92944d05284cd3f
Kuba PrusakiewiczPC <jakubprusakiewicz@gmail.com>
2020-10-13 23:14:40 +0200

    feat player color picker

---
+++ ML_Cars/Assets/CarSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarSpawner : MonoBehaviour
+{
+    public GameObject Car;
+    // Start is called before the first frame update
+    void Start()
+    {
+        var playerColor = PlayGame.playerColor;
+        if (playerColor != -1)
+        {
+            Car.GetComponent<Renderer>().material.color = Color.HSVToRGB(playerColor, 1, 1);;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- ML_Cars/Assets/PlayGame.cs
+++ ML_Cars/Assets/PlayGame.cs
@@ -2,19 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 public class PlayGame : MonoBehaviour
 {
-    public Button Button;
+    public Button Button_Play;
+    public Button Button_Quit;
+    public Slider Colour_picker;
+
+    public GameObject Car;
+    static public float playerColor = -1;
+    public Material CarColour;
     // Start is called before the first frame update
     void Start()
     {
-        Button.onClick.AddListener(TaskOnClick);
+        Button_Play.onClick.AddListener(PlayGameTaskOnClick);
+        Button_Quit.onClick.AddListener(QuitGameTaskOnClick);
+        Colour_picker.onValueChanged.AddListener(CarColourChanged);
     }
-    private void TaskOnClick()
+
+    private void CarColourChanged(float colourValue)
+    {
+        var color = Color.HSVToRGB(colourValue, 1, 1);
+       // CarColour.SetColor("user_color",Color.magenta);
+        Car.GetComponent<Renderer>().material.color = color;
+        playerColor = colourValue;
+    }
+
+    private void PlayGameTaskOnClick()
     {
         SceneManager.LoadScene("MainScene", LoadSceneMode.Single);
+    }
+    private void QuitGameTaskOnClick()
+    {
+        Application.Quit();
     }
     // Update is called once per frame
 }
